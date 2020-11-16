28327cf42b7e9669154c81cd904d3007c13da71f
DESKTOP-BDOHPTU\PC <eng.moh.sal.91@gmail.com>
2020-11-16 16:45:41 +0200

    updates

---
+++ app/src/main/java/com/vpc3/personalexpensesapp/activites/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.vpc3.personalexpensesapp.activites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import com.vpc3.personalexpensesapp.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    ImageView logo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        logo = findViewById(R.id.appLogo);
+
+        Animation a = AnimationUtils.loadAnimation(this,R.anim.rotote);
+        a.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) { }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+              //  Intent i = new Intent(SplashActivity.this,MainActivity.class);
+               // startActivity(i);
+               // finish();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) { }
+        });
+        logo.setAnimation(a);
+    }
+}
